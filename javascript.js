8cbc52faf2eb456dc118328502818ad01468a5db
JSoot8 <JSoot8@gmail.com>
2025-11-23 14:48:01 +0200

    Add a button to change amount of squares
    The button prompts the user and asks for the desired size of a side of the grid in squares and then removes the previous grid replacing it with a new one the same total size but with the desired amount of squares.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,6 @@
 let sketchPad = document.querySelector('.sketch-pad');
 
-let counter = 0;
-while(counter != 256) {
-    let newSquare = document.createElement('div');
-    newSquare.classList.add('sketch-square');
-    sketchPad.appendChild(newSquare);
-    counter++
-}
+sketchPadGenerate(16);
 
 sketchPad.addEventListener('mouseover', (event) => {
     event.target.style.backgroundColor = 'rgba(211, 211, 211, 1)';
@@ -15,3 +9,29 @@ sketchPad.addEventListener('mouseover', (event) => {
 sketchPad.addEventListener('mouseout', (event) => {
     event.target.style.backgroundColor = 'rgba(211, 211, 211, 0.3)';
 });
+
+let resizeBtn = document.querySelector('.resize-button');
+resizeBtn.addEventListener('click', () => {
+    let sideAmount = prompt('How many squares per side?');
+    sketchPadGenerate(parseInt(sideAmount));
+});
+
+function sketchPadGenerate (sideSize) {
+    let counter = 0;
+    while(sketchPad.children.length != 0){
+        sketchPad.removeChild(sketchPad.lastChild);
+    }
+
+    sketchPad.style.setProperty('--grid-sides', sideSize);
+
+    //square space is calculated by multiplying its side by itself.
+    while(counter != sideSize ** 2) {
+        let newSquare = document.createElement('div');
+        newSquare.classList.add('sketch-square');
+        sketchPad.appendChild(newSquare);
+        counter++
+    }
+}
+
+//generate amount of squares COMPLETE
+//
