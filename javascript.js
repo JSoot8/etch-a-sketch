e8673dfd70dab386e2d1db4cf152a0a4296e545b
JSoot8 <JSoot8@gmail.com>
2025-11-23 13:41:51 +0200

    Add a hover effect over each square in the grid

--- javascript.js
+++ javascript.js
@@ -7,3 +7,11 @@ while(counter != 256) {
     sketchPad.appendChild(newSquare);
     counter++
 }
+
+sketchPad.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = 'rgba(211, 211, 211, 1)';
+});
+
+sketchPad.addEventListener('mouseout', (event) => {
+    event.target.style.backgroundColor = 'rgba(211, 211, 211, 0.3)';
+});
